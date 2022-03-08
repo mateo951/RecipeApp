0ec87833e2f0c9643754346452c2602d695cade0
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-08 14:56:57 -0500

    Add devise configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
-
   # GET /users or /users.json
   def index
     @users = User.all
@@ -8,6 +6,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
