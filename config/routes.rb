f0cb4f57e5a095ed2d31cdea2662690f8fbb1bc7
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-08 16:26:31 -0500

    Add hotfix for devise logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "users#index"
+  resources :users
 end
