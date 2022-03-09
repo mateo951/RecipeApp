97711ae1262d98f51472aa59e23bc40e38ff5929
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-03-09 19:52:04 +0200

    Remove the validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: :user_id
-  validates :name, presence: true
 
 end
