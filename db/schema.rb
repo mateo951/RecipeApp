59760eab965a88e4a998ce4e3b712533708b2b1d
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-03-09 20:29:07 +0200

    Update recipe_food models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_231048) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_182226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.string "measurment_unit"
+    t.string "measurement_unit"
     t.decimal "price", precision: 10, scale: 2
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -26,8 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_231048) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
+    t.bigint "recipe_id"
+    t.bigint "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -56,5 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_231048) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
