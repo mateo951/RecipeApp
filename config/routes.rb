c93e2ad9a2d93151b11a8c0f1233c172a979adc4
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-09 18:30:48 -0500

    Add recipe wireframe setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root "users#index"
+  root "recipes#index"
   resources :users
+  resources :recipes
 end
