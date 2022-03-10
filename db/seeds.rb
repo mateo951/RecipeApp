b38c7f1d5e9cddecea27572d2a8899edadc6f019
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-09 19:23:01 -0500

    Add wireframe view for Recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+2.times do |i|
+	user = User.create(name: "User ##{i+1}")
+	3.times do |j|
+	    recipes = Recipe.create(name: "Awesome Recipe", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop", user_id: user.id, public: true)
+	end
+end
